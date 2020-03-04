f615dcc7135e50e5c1d42cd760f6c845eb8406c8
Anindya Chakraborty <anindya100c@gmail.com>
2020-03-04 10:30:36 +0530

    initial commit

---
+++ src/main/java/com/quantab/chthospital/ChthospitalApplication.java
@@ -0,0 +1,13 @@
+package com.quantab.chthospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChthospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChthospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantab/chthospital/ChthospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantab.chthospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChthospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
